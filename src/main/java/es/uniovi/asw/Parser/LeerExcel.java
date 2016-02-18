9988ca997f9e4406d3d85fdd623b621b8f6d1ce2
mfenix360 <dariosuarez_@hotmail.com>
2016-02-18 12:53:20 +0100

    arreglo leer excel y manipulacion datos

--- src/main/java/es/uniovi/asw/Parser/LeerExcel.java
+++ src/main/java/es/uniovi/asw/Parser/LeerExcel.java
@@ -21,40 +21,71 @@ import es.uniovi.asw.DBUpdate.model.Usuario;
 
 public class LeerExcel {
 
-	public void leerFichero(String fichero) throws IOException{
-	File myFile = new File(fichero+".xlsx"); 
-	FileInputStream fis = new FileInputStream(myFile);
-	List<Usuario> data = new ArrayList<Usuario>();
-
-	XSSFWorkbook myWorkBook = new XSSFWorkbook (fis);
-	XSSFSheet mySheet = myWorkBook.getSheetAt(0);
- 
-	  Iterator<Row> rowIterator = mySheet.iterator();
+	public List<Usuario> leerFichero(String fichero) throws IOException{
+		
+		File myFile = new File(fichero+".xlsx"); 
+		FileInputStream fis = new FileInputStream(myFile);
+		List<Usuario> data = new ArrayList<Usuario>();
+	
+		XSSFWorkbook myWorkBook = new XSSFWorkbook (fis);
+		XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+	 
+		  Iterator<Row> rowIterator = mySheet.iterator();
+		  
+		  while (rowIterator.hasNext()) {
+	        Row row = rowIterator.next();
+	        
+	        Iterator<Cell> cellIterator = row.cellIterator();
+	        Usuario user= new Usuario();
+	        int cont=0;
+	        
+	        while (cellIterator.hasNext()) {
+	            Cell cell = cellIterator.next();
+	            
+	            if(cell.getRow().getRowNum()>0){
+	            	
+	              switch (cell.getCellType()) {
+		            case Cell.CELL_TYPE_STRING:
+		            	
+		            	if(cont==0){
+		            		
+		            		user.setNombre(cell.getStringCellValue());
+		            		cont++;
+		            		
+		            	}else if(cont==1){
+		            		
+		            		user.setEmail(cell.getStringCellValue());
+		            		cont++;
+		            		
+		            	}else{
+		            		
+		            		user.setNIF(cell.getStringCellValue());
+		            		
+		            	}
+		            	
+		                break;
+		                
+		            case Cell.CELL_TYPE_NUMERIC:
+		            	
+		            	
+		            	user.setCodColElectoral((int)cell.getNumericCellValue());
+		                break;
+		             default:
+		            	 break;
+		            }
+	
+	            }
+		          
+	        }
+	        
+	        if(user.getLogin()!=null){
+	        	data.add(user);
+	        }
+	        
+	        System.out.println("");
+		  }
+		  
+		  return data;
 	  
-	  while (rowIterator.hasNext()) {
-        Row row = rowIterator.next();
-        
-        Iterator<Cell> cellIterator = row.cellIterator();
-        while (cellIterator.hasNext()) {
-            Cell cell = cellIterator.next();
-            
-            switch (cell.getCellType()) {
-            case Cell.CELL_TYPE_STRING:
-                System.out.print(cell.getStringCellValue() + "\t");
-                break;
-                
-            case Cell.CELL_TYPE_NUMERIC:
-                System.out.print(cell.getNumericCellValue() + "\t");
-                break;
-            case Cell.CELL_TYPE_BOOLEAN:
-                System.out.print(cell.getBooleanCellValue() + "\t");
-                break;
-             default:
-            }
-
-        }
-        System.out.println("");
-	  }
-
 	}
 }
--- src/test/java/es/uniovi/asw/TestBD.java
+++ src/test/java/es/uniovi/asw/TestBD.java
@@ -22,7 +22,7 @@ public class TestBD {
 		
 	}*/
 	
-	private Usuario user = new Usuario("Dario","UO230766@uniovi.es","53548918L",25);
+	private Usuario user = new Usuario("Dario Suarez","UO230766@uniovi.es","53548918L",25);
 	private Usuario user1 = new Usuario("Victor","correo1","69853214T",12);
 	private Usuario user2 = new Usuario("Pepe","correo2","87485963J",30);
 	private Usuario user3 = new Usuario("Juan","correo3","71503695A",02);
@@ -30,10 +30,13 @@ public class TestBD {
 	@Test
 	public void testADD() {
 		
+		
 		assertTrue(Factories.persistence.createUsuarioDao().save(user));
 		assertTrue(Factories.persistence.createUsuarioDao().save(user1));
 		assertTrue(Factories.persistence.createUsuarioDao().save(user3));
 		assertTrue(Factories.persistence.createUsuarioDao().save(user2));
+		
+		System.out.println(user.toString());
 		
 		assertFalse(Factories.persistence.createUsuarioDao().save(user2));
 		assertFalse(Factories.persistence.createUsuarioDao().save(user1));
